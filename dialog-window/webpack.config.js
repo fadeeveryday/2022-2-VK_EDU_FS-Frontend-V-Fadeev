58cd3758732acc11c3c9e0944a2972e3d03d9a5b
fadeeveryday <94104359+fadeeveryday@users.noreply.github.com>
2022-11-03 14:16:15 +0300

    add html and webpack

---
+++ dialog-window/webpack.config.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const SRC_PATH = path.resolve(__dirname, '');
+//const BUILD_PATH = path.resolve(__dirname, 'build');
+
+module.exports = {
+    //context: SRC_PATH,
+    entry: {
+        index: path.resolve(__dirname, '/js/script.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        },
+                    },
+                ],
+            },
+            {
+                test: /shadow\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'css-loader'
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'style.css',
+            template: path.resolve(__dirname, './css/style.css')
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, './index.html')
+        })
+    ]
+};
