cd6f223f21b1af4ec8e20721bc8d42b8db3d9310
fadeeveryday <94104359+fadeeveryday@users.noreply.github.com>
2022-11-05 12:01:49 +0300

    undate script.js

--- dialog-window/js/script.js
+++ dialog-window/js/script.js
@@ -1,8 +1,11 @@
-//import './css/style.css';
 const form = document.querySelector('form');
 const input = document.querySelector('form-input');
-const chat = document.querySelector('.chat')
-const back_button = document.querySelector('.header-wrapper__button')
+const chat = document.querySelector('.chat');
+const back_button = document.querySelector('.header-wrapper__button');
+const img = new Image();
+img.src = './img/avatar.jpg';
+page.appendChild(img);
+
 
 form.addEventListener('submit', handleSubmit.bind(this));
 document.addEventListener('DOMContentLoaded', getMesagesFromLocalStorage.bind(this))
